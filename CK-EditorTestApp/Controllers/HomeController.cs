e41fdcbac8eefe6a12a79c06c6e2b8a01fa59abe
hlink <hlink@DESKTOP-8K8MQUP>
2024-03-07 19:36:08 +0530

    Add project files.

---
+++ CK-EditorTestApp/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using CK_EditorTestApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace CK_EditorTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadImage(List<IFormFile> files)
+        {
+            try
+            {
+                string directoryPath = Path.Combine(_env.WebRootPath, "Image");
+
+                if (!Directory.Exists(directoryPath))
+                {
+                    // If the directory doesn't exist, create it
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                foreach (IFormFile photo in files)
+                {
+                    if (photo.Length > 0)
+                    {
+                        string uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                        string filePath = Path.Combine(directoryPath, uniqueFileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            photo.CopyTo(stream);
+                        }
+
+                        string filepath = "https://localhost:44346/" + "Image/" + uniqueFileName;
+                    }
+                }
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, error = ex.Message });
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CK-EditorTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CK_EditorTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
